8244eaf65cb19f202b1048030e12034b330b2275
Naima <naimakarzouz@gmail.com>
2019-09-16 14:04:17 -0400

    Created basic React Home page
    Added bootstrap within /src for formatting; created basic components for list.

---
+++ sd-food-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListOfIngredients from './components/ListOfIngredients.js';
+
+class App extends React.Component {
+  render()
+  {
+    return (
+    <div>
+    <title>Yum.me</title>
+    <h1><center>Yum.me!</center></h1>
+    <h2><center>Your guide to economic eating</center></h2>
+    <h4>What's in your fridge?</h4>
+    <div><ListOfIngredients /></div>
+    </div>
+    );
+  }
+}
+export default App;

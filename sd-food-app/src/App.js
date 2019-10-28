93f8732497f677538707288007f84a44e7444871
Naima <naimakarzouz@gmail.com>
2019-10-28 15:14:22 -0400

    10/28/2019: Minor Appearance Changes

--- sd-food-app/src/App.js
+++ sd-food-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bootstrap from './bootstrap/css/bootstrap.css';
 import ListOfIngredients from './components/ListOfIngredients.js';
 import Food2forkAPITest from './components/food2forkAPITest.js';
 
@@ -8,19 +8,33 @@ class App extends React.Component {
   render()
   {
     return (
-    <div class="container">
+    <div className="container">
+    <h1>
+      <center>Yum.me!</center>
+    </h1>
+    <h2>
+      <center>Your guide to economic eating</center>
+    </h2>
+
+    <div id="contents">
       <title>Yum.me</title>
-      <h1>
-        <center>Yum.me!</center>
-      </h1>
-      <h2>
-        <center>Your guide to economic eating</center>
-      </h2>
-      <h4>What's in your fridge?</h4>
-      <div><ListOfIngredients /> </div>
-      <div><Food2forkAPITest /></div>
+      <div class="row">
+        <div class="col">
+          <h4>What's in your fridge?</h4>
+          <div><ListOfIngredients /> </div>
+          <br></br>
+        </div>
+        <div class="col">
+        <div id="Recipe">
+        A Recipe goes here.
+        </div>
+        </div>
+      </div>
+    </div>
     </div>
     );
   }
 }
+
+
 export default App;
--- sd-food-app/src/components/ListOfIngredients.js
+++ sd-food-app/src/components/ListOfIngredients.js
@@ -6,7 +6,14 @@ class ListOfIngredients extends React.Component {
   {
     return (
     <div>
-      <input type="checkbox" value="Milk" /> Milk <br></br>
+      <input class="allCheckboxes" type="checkbox" value="Milk" /> Milk <br></br>
+      <input class="allCheckboxes" type="checkbox" value="Eggs" /> Eggs <br></br>
+      <input class="allCheckboxes" type="checkbox" value="Bread" /> Bread <br></br>
+      <input class="allCheckboxes" type="checkbox" value="Ketchup" /> Ketchup <br></br>
+      <input class="allCheckboxes" type="checkbox" value="Sugar" /> Sugar <br></br>
+      <br></br>
+      <button type="button" class="btn btn-info" onClick={testfunction}>Submit</button>
+
     </div>
     );
   }
@@ -18,4 +25,24 @@ class ListOfIngredients extends React.Component {
 
   }
 }
+
+/*
+gets the element for the Recipe and changes the inner text of the rightmost column to the recipe
+*/
+function testfunction(){
+    var recipeEdit = document.getElementById("Recipe");
+
+    var checkedValue = null;
+    var inputElements = document.getElementsByClassName("allCheckboxes");
+
+    for(var i =0; inputElements[i]; ++i){
+      if(inputElements[i].checked){
+        checkedValue = inputElements[i].value;
+        break;
+      }
+    }
+    console.log(checkedValue);
+  //  recipeEdit.innerHTML = "Test";
+  }
+
 export default ListOfIngredients;

d478dafc5fa90e04e01a791edbe9e29f9d815dd3
Naima <naimakarzouz@gmail.com>
2019-11-08 11:04:27 -0500

    Minor Syntax Error Fixes which kept program from compiling
    sorting

--- sd-food-app/src/components/Main.js
+++ sd-food-app/src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Bootstrap from '../bootstrap/css/bootstrap.css';
-import ListOfIngredients from './components/ListOfIngredients.js';
-import Food2forkAPITest from './components/food2forkAPITest.js';
+import ListOfIngredients from '../components/ListOfIngredients.js';
+import Food2forkAPITest from '../components/food2forkAPITest.js';
 
 class Main extends React.Component {
 
@@ -10,14 +10,11 @@ class Main extends React.Component {
   render()
   {
     return (
-    <ListOfIngredients> <ListOfIngredients/>
-    <Food2forkAPITest> <Food2forkAPITest/>
-
+      <div>
+  <ListOfIngredients />
+  <Food2forkAPITest />
+    </div>
     );
   }
 }
-
-
-
-
 export default Main;

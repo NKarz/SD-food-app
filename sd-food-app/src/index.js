b3569c6d44217b5990546aa180cd8113da4e6ee0
Carson <cedwar99@uncc.edu>
2019-11-19 15:42:00 -0500

    initialized firebase in the app

--- sd-food-app/src/index.js
+++ sd-food-app/src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import {BrowserRouter, Route} from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
 
 import Food2forkAPITest from './components/food2forkAPITest.js';
 import SignIn from './components/SignIn.js';
 import Main from './components/Main.js';
 import SignUp from './components/SignUp.js';
+
+var config = {
+    apiKey: "AIzaSyDVteIZdlwMtaNINA5ZmkNvGgUYQ9FfOn8",
+    authDomain: "yumme-a9bc0.firebaseapp.com",
+    databaseURL: "https://yumme-a9bc0.firebaseio.com",
+    projectId: "yumme-a9bc0",
+    storageBucket: "yumme-a9bc0.appspot.com",
+    messagingSenderId: "343478442579",
+    appId: "1:343478442579:web:6d5fd0cc58807cb9b944a6",
+    measurementId: "G-5FVK78GE90"
+  };
+
+firebase.initializeApp(config);
+
+
+
 
 const routing = (
   <BrowserRouter>

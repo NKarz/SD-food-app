4469f3f83ce1c7a13d2f1fecc06501974a90eca0
Naima <naimakarzouz@gmail.com>
2019-11-30 18:37:36 -0500

    Changed API over to Edemam - Unformatted, but correct API Pull

--- sd-food-app/src/components/food2forkAPITest.js
+++ sd-food-app/src/components/food2forkAPITest.js
@@ -4,7 +4,7 @@ import Bootstrap from '../bootstrap/css/bootstrap.css';
 class Food2forkAPITest extends React.Component {
   constructor(props){
     super(props);
-    this.state={recipes: [],
+    this.state={recipes: []
     };
   }
 
@@ -13,35 +13,52 @@ class Food2forkAPITest extends React.Component {
     return (
       <div className="App">
         <div>
-          {
-            console.log("Ingredients?" +  this.props.ingredients)
-          }
-              <button onClick={() => this.getRecipes()}>Show recipes</button>
+              {this.getRecipes()}
+            //  {this.state.recipes.map(res => <div> <h2><a href={res.source_url}> {res.title} </a> </h2> <img src={res.image_url}/> </div>)}
+              {this.state.recipes.map(result => <div>{result.recipe.label}</div>)}
         </div>
 
         <div>
-          {this.state.recipes.map(res => <div> <h2><a href={res.source_url}> {res.title} </a> </h2> <img src={res.image_url}/> </div>)}
         </div>
       </div>
     );
   }
 
  getRecipes(){
-  var api_key = '51851afa92a60432329217d5876bdf01';
-  var base_url;
+   var app_id = '131489dd';
+   var app_key = '570a130e3713f57617735f17ece1d187';
 
-  base_url = 'https://www.food2fork.com/api/search?key=' + api_key + '&q=' + this.props.ingredients;
+   var base_url;
 
-  fetch(base_url)
-   .then((response) => {
-     return response.json();
-   })
-   .then(data => {
-     this.setState({
-       recipes: data.recipes
-   });
-   console.log("DATA " + data);
-   })
+   base_url = 'https://api.edamam.com/search?q=' + this.props.ingredients + "&app_id=" + app_id + "&app_key=" + app_key;
+
+   fetch(base_url)
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({
+        recipes: data["hits"]
+    });
+     console.log("DATA " + data);
+    })
+
+   //as of November 30th, Food2Fork has expired
+  // var api_key = '51851afa92a60432329217d5876bdf01';
+  // var base_url;
+  //
+  // base_url = 'https://www.food2fork.com/api/search?key=' + api_key + '&q=' + this.props.ingredients;
+  //
+  // fetch(base_url)
+  //  .then((response) => {
+  //    return response.json();
+  //  })
+  //  .then(data => {
+  //    this.setState({
+  //      recipes: data.recipes
+  //  });
+  //  // console.log("DATA " + data);
+  //  })
+
+
 }
 }
 

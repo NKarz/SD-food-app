f7feda696d87cb326dcfdfb143efd86166549f3b
Yentzameng Yang <yengyang7009@gmail.com>
2019-12-09 12:18:20 -0500

    update
    update my sort time and recipes

--- sd-food-app/src/components/SortRecipe.js
+++ sd-food-app/src/components/SortRecipe.js
@@ -32,15 +32,7 @@ SortRecipes('example.json')
 
 */
 
-setInterval(function(){
-var main = document.getElementById( 'listofrec2' );
 
-[].map.call( main.children, Object ).sort( function ( a, b ) {
-    return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
-}).forEach( function ( elem ) {
-    main.appendChild( elem );
-});
-}, 3000);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
